75f5f139bba11706d65ae1e64ec78446b83b7317
xczylt <78222012+xczylt@users.noreply.github.com>
2024-11-16 07:46:19 +0800

    Update 1.js

--- 1.js
+++ 1.js
@@ -3,7 +3,6 @@
         const phone = '17608462601';
         const wxpusherAppToken = 'AT_42SgggbnnXdnm3gzQPiYcbShLGTRYFgh';
         const wxpusherUIDs = ['UID_kJP2XxI6RxcVtjt4W3Ve1lRW5Zzp'];
-
 // 显示信息框
 function showInfoBox(type, message) {
     const infoBox = document.getElementById('infoBox');
@@ -16,10 +15,9 @@ function showInfoBox(type, message) {
     infoBoxIcon.classList.add(type);
     infoBoxMessage.textContent = message;
 
-    // 设置一个变量用于控制显示时间，初始化为5000毫秒（5秒），方便后续根据不同提示灵活调整
     let displayTime = 5000;
     if (type ==='success' && message === '正在通知车主... 1') {
-        displayTime = 4000; // 如果是“正在通知车主...”这个提示，设置显示4秒
+        displayTime = 2000; // 展示时间调整为2秒
     }
     setTimeout(() => {
         infoBox.classList.remove('show');
@@ -41,12 +39,11 @@ function notifyOwnerWithCustomMessage() {
     const customMessage = document.getElementById('customMessage').value.trim();
     const messageToSend = customMessage.length > 0? customMessage : "您好，有人需要您挪车，请及时处理。 6";
 
+    const notifyPromise = notifyOwner(messageToSend);
     showInfoBox('success', '正在通知车主... 1');
-
-    setTimeout(() => {
-        notifyOwner(messageToSend);
+    notifyPromise.then(() => {
         lastNotificationTime = Date.now(); // 更新上次通知时间
-    }, 4000);  // 延迟4秒，模拟通知过程
+    });
 }
 
 function notifyOwner(messageToSend) {
@@ -57,22 +54,22 @@ function notifyOwner(messageToSend) {
         uids: wxpusherUIDs
     };
 
-    fetch("https://wxpusher.zjiecode.com/api/send/message", {
+    return fetch("https://wxpusher.zjiecode.com/api/send/message", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(bodyContent)
     })
-  .then(response => response.json())
-  .then(data => {
+ .then(response => response.json())
+ .then(data => {
         if (data.code === 1000) {
             setTimeout(() => {
                 showInfoBox('success', '已成功通知车主前来移车，请稍后等待！ 11');
-            }, 500); // 延迟500毫秒（半秒）显示成功通知车主的提示
+            }, 500);
         } else {
             showInfoBox('error', `通知发送失败: ${data.msg || '未知错误 0'}`);
         }
     })
-  .catch(error => {
+ .catch(error => {
         showInfoBox('error', `通知发送失败，请稍后重试。错误: ${error.message} 02`);
     });
 }
